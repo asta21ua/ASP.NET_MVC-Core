eebb4017f284f7eb959811b59dc20cf5de7a69da
asta21ua <asta21ua@yahoo.com>
2019-03-24 23:17:38 +0400

    Creating invitation

---
+++ PartyInvitatyion_WebApp/PartyInvitatyion_WebApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvitatyion_WebApp.Models;
+
+namespace PartyInvitatyion_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView");
+            
+        }
+
+    }
+}
