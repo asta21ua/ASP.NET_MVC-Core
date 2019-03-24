7c2deead20b2d0e79c593fa8dc10865d111457d1
asta21ua <asta21ua@yahoo.com>
2019-03-24 15:05:24 +0400

    Creating new webapp

---
+++ PartyInvites/Party_Invites_wepApp/Party_Invites_wepApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Party_Invites_wepApp.Models;
+
+namespace Party_Invites_wepApp.Controllers
+{
+    public class HomeController : Controller
+    {public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ PartyInvites/Party_Invites_wepApp/Party_Invites_wepApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Party_Invites_wepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
