056a049da13806a5a9b108ed6bd27b638eca7abd
asta21ua <asta21ua@yahoo.com>
2019-03-24 16:38:41 +0400

    Adding the ViewBag object in HomeController.cs and in view/home MyView

--- PartyInvites/Party_Invites_WebApp/Party_Invites_WebApp/Controllers/HomeController.cs
+++ PartyInvites/Party_Invites_WebApp/Party_Invites_WebApp/Controllers/HomeController.cs
@@ -9,9 +9,16 @@ using Party_Invites_wepApp.Models;
 namespace Party_Invites_wepApp.Controllers
 {
     public class HomeController : Controller
-    {public ViewResult Index()
-        {
+    {
+       public ViewResult Index() //adding data of view in HomeController.cs using ViewBag object 
+                  
+       {
+            int hour = DataTime.Now.Hour;
+            ViewBag.Greeting  = hour <12? "Good Morning": "Good Afternoon";
             return View ("MyView");
-        }
+       }
+       
+    
+
     }
 }
