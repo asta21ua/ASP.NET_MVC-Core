457e9aaee9578ce3894e6500f1d5ea2fab6825bd
asta21ua <asta21ua@yahoo.com>
2019-05-16 23:42:34 +0400

    New project was Added

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+                results.Add(string.Format("Name: {0}, Price {1}", name, price));
+            }
+            return View(results);
+
+        }
+
+    }
+}
