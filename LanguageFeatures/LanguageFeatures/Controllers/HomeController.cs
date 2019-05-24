d68e2a8012903a1be9e7b27f0949091f48fc6ce4
asta21ua <asta21ua@yahoo.com>
2019-05-24 22:48:42 +0400

    additing HomeController and Product

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -10,20 +10,29 @@ namespace LanguageFeatures.Controllers
     public class HomeController : Controller
     {
         public ViewResult Index()
+
         {
-            List<string> results = new List<string>();
-
-            foreach (Product p in Product.GetProducts())
-
+            Dictionary<string, Product> products = new Dictionary<string, Product>
             {
-                string name = p?.Name?? "No Name";
-                decimal? price = p?.Price?? 0;
-                string relatedName = p?.Related?.Name?? "None";
-                results.Add(string.Format("Name: {0}, Price {1}, Related: {2} ", name, price, relatedName));
-            }
-            return View(results);
+                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+
+            };
+            return View("Index", products.Keys);
+
+            //List<string> results = new List<string>();
+
+            //foreach (Product p in Product.GetProducts())
+            //{
+            //    string name = p? .Name ?? "No Name";
+            //    decimal? price = p? .Price ?? 0;
+            //    string relatedName = p? .Related? .Name ?? "<None>";
+            //    results.Add(string.Format("Name: {0}, Price {1}, Related: {2} ", name, price, relatedName));
+            //}
+            //return View(results);
 
         }
+
 
     }
 }
--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -8,14 +8,17 @@ namespace LanguageFeatures.Models
     public class Product
     {
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; set; }
 
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 275M
             };
 
