bbd271a31d0d4007a6e68d30f6cefa5906270c1f
asta21ua <asta21ua@yahoo.com>
2019-06-11 22:42:24 +0400

    ReadOnley Automatically Implemented Properties

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -7,25 +7,28 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        public Product(bool stock = true)                     //Read-Only Automatically Implemented Properties
+        {
+            InStock = stock;
+        }
         public string Name { get; set; }
-        // public string Category { get; set; } = "Watersports";
+        public string Category { get; set; } = "Watersports"; //Auto-Implemented Property Initializer
         public decimal? Price { get; set; }
         public Product Related { get; set; }
-        // public bool InStock { get; } = true;
-
+        public bool InStock { get; }                         //Read-Only Automatically Implemented Properties
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
-                //Category = "Water Craft",
+                Category = "Water Craft",
                 Price = 275M
             };
-
-            Product lifejacket = new Product
+             
+            Product lifejacket = new Product(false)         //Read-Only Automatically Implemented Properties
             {
                 Name = "LifeJecket",
-                Price = 48.23M
+                Price = 48.95M
             };
 
             kayak.Related = lifejacket;
