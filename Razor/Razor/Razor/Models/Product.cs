2ec87843b086f87427acdebd8fb4fb8992b55a3e
asta21ua <asta21ua@yahoo.com>
2019-08-28 23:57:31 +0400

    Three new folder were added

---
+++ Razor/Razor/Razor/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+        public int Price { get; set; }
+        public int Category { get; set; }
+
+    }
+}
