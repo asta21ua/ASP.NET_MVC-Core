e54580a805e555b180bc1ee2aac0c572bfc21bc5
asta21ua <asta21ua@yahoo.com>
2019-09-01 16:18:08 +0400

    Enumerating an Array

--- Razor/Razor/Razor/Controller/HomeController.cs
+++ Razor/Razor/Razor/Controller/HomeController.cs
@@ -9,18 +9,35 @@ namespace Razor.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        //public ViewResult Index()
+        //{
+        //    Product myProduct = new Product
+        //    {
+        //        ProductID = 1,
+        //        Name = "Kayak",
+        //        Description = "A boat for one person",
+        //        Category = "Watersports",
+        //        Price = 275M
+        //    };
+        //    ViewBag.StockLevel = 2;
+        //    return View(myProduct);
+        //}
+
+        public IActionResult Index() // Enumerating an Array
         {
-            Product myProduct = new Product
+            Product[] array =
             {
-                ProductID = 1,
-                Name = "Kayak",
-                Description = "A boat for one person",
-                Category = "Watersports",
-                Price = 275M
+                new Product {Name ="Kayak", Price = 275M },
+                new Product {Name = "Lifejacket", Price = 48.65M },
+                new Product {Name = "Soccer ball", Price = 15.56M },
+                new Product {Name = "Corner flag", Price = 30.45M },
+
             };
-            ViewBag.StockLevel = 2;
-            return View(myProduct);
+            return View(array);
+
         }
     }
 }
+
+
+
