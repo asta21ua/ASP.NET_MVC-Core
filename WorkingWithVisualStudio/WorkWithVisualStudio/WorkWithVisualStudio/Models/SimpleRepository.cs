069487f76010c02de0281605c490dad1b28c572f
asta21ua <asta21ua@yahoo.com>
2019-09-03 00:20:16 +0400

    Creating Simple Repository.cs class

--- WorkingWithVisualStudio/WorkWithVisualStudio/WorkWithVisualStudio/Models/SimpleRepository.cs
+++ WorkingWithVisualStudio/WorkWithVisualStudio/WorkWithVisualStudio/Models/SimpleRepository.cs
@@ -14,14 +14,24 @@ namespace WorkWithVisualStudio.Models
 
         public SimpleRepository()
         {
-            var iinitialItems = new[]
+            var initialItems = new[]
+               {
+            new Product {Name = "Kayak", Price=254M },
+            new Product { Name = "Lifejacket", Price = 56.42M },
+            new Product { Name = "Soccer ball", Price = 15.4M},
+            new Product { Name = "Corner Flag", Price = 20.4M }
+            };
+
+
+            foreach (var p in initialItems)
             {
+                AddProduct(p);
+            }
+        }
 
-
-             }
-        };
+        public IEnumerable<Product> Products => products.Values;
+        public void AddProduct(Product p) => products.Add(p.Name, p);
     }
 
-    p
-
 }
+
