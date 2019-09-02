a9eba7afa60d61ded326ad101b4b527945ad6901
asta21ua <asta21ua@yahoo.com>
2019-09-02 23:59:35 +0400

    Creating new project Working with Visual Studio

---
+++ WorkingWithVisualStudio/WorkWithVisualStudio/WorkWithVisualStudio/Models/SimpleRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        public static SimpleRepository SharedRepository => new SimpleRepository();
+
+        public SimpleRepository()
+        {
+            var iinitialItems = new[]
+            {
+
+
+             }
+        };
+    }
+
+    p
+
+}
